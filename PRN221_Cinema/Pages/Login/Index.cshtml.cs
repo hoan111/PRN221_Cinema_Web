f25716ed67e65724606cad45ddd590c66fc00c1a
hoan111 <hoan1001@gmail.com>
2023-03-25 00:19:16 +0700

    Add login/logout function

---
+++ PRN221_Cinema/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN221_Cinema.DAO;
+using PRN221_Cinema.Models;
+
+namespace PRN221_Cinema.Pages.Login
+{
+
+    public class IndexModel : PageModel
+    {
+        private readonly CinemaDBContext _context;
+
+        [BindProperty]
+        public LoginDAO loginDAO { get; set; }
+        [BindProperty]
+        public string errMsg { get; set; }
+        public IndexModel(CinemaDBContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            ModelState.Remove("errMsg");
+            if (ModelState.IsValid)
+            {
+                Person person = _context.Persons.Where(p => p.Email.Equals(loginDAO.email) && p.Password.Equals(loginDAO.password)).FirstOrDefault();
+                if (person != null)
+                {
+                    HttpContext.Session.SetString("FullName", person.Fullname);
+                    Response.Redirect("/");
+                }
+                else
+                {
+                    errMsg = "Thông tin đăng nhập không đúng";
+                }
+            }
+        }
+    }
+}
