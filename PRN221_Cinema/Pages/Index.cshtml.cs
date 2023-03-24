aff239a84efd74426d1817621dd40b15e78171ba
hoan111 <hoan1001@gmail.com>
2023-03-25 03:08:34 +0700

    Added query to show average rating point, fix image view on list films

--- PRN221_Cinema/Pages/Index.cshtml.cs
+++ PRN221_Cinema/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using PRN221_Cinema.Models;
 using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
 
@@ -26,6 +27,39 @@ namespace PRN221_Cinema.Pages
             _context = context;
         }
 
+        //public void OnGet(int? genreId, string? searchStr)
+        //{
+        //    IQueryable<Movie> query = _context.Movies.Include(x => x.Genre);
+
+        //    if (genreId != null)
+        //    {
+        //        query = query.Where(m => m.GenreId == genreId);
+        //    }
+
+        //    if (!string.IsNullOrEmpty(searchStr))
+        //    {
+        //        //query = query.Where(m => EF.Functions.Like(m.Title, $"%{searchStr}%"));
+        //        query = query.Where(m => m.Title.Contains(searchStr));
+        //    }
+        //    //Process calculate averrage point each film
+        //    var ratingQuery = _context.Rates.GroupBy(r => r.MovieId)
+        //        .Select(g => new { MovieId = g.Key, RatingPoint = g.Average(r => r.NumericRating) });
+        //    query = query.LeftJoin(ratingQuery, m => m.MovieId, r => r.MovieId, (m, r) => new Movie
+        //    {
+        //        MovieId = m.MovieId,
+        //        Description = m.Description,
+        //        Genre = m.Genre,
+        //        GenreId = m.GenreId,
+        //        Image = m.Image,
+        //        Rates = m.Rates,
+        //        Title = m.Title,
+        //        Year = m.Year,
+        //        RatingPoint = r.RatingPoint,
+        //    });
+        //    movies = query.ToList();
+        //    genres = _context.Genres.ToList();
+        //}
+
         public void OnGet(int? genreId, string? searchStr)
         {
             IQueryable<Movie> query = _context.Movies.Include(x => x.Genre);
@@ -40,6 +74,25 @@ namespace PRN221_Cinema.Pages
                 //query = query.Where(m => EF.Functions.Like(m.Title, $"%{searchStr}%"));
                 query = query.Where(m => m.Title.Contains(searchStr));
             }
+            //Process calculate averrage point each film
+            var ratingQuery = _context.Rates
+                .GroupBy(r => r.MovieId)
+                .Select(g => new { MovieId = g.Key, RatingPoint = g.Average(r => r.NumericRating) });
+
+            query = query
+                .GroupJoin(ratingQuery, m => m.MovieId, r => r.MovieId, (m, r) => new { Movie = m, Rating = r })
+                .SelectMany(x => x.Rating.DefaultIfEmpty(), (x, r) => new Movie
+                {
+                    MovieId = x.Movie.MovieId,
+                    Description = x.Movie.Description,
+                    Genre = x.Movie.Genre,
+                    GenreId = x.Movie.GenreId,
+                    Image = x.Movie.Image,
+                    Rates = x.Movie.Rates,
+                    Title = x.Movie.Title,
+                    Year = x.Movie.Year,
+                    RatingPoint = r.RatingPoint
+                });
             movies = query.ToList();
             genres = _context.Genres.ToList();
         }
